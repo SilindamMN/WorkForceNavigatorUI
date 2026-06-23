768861898b0c8ad867bc6920516cf5071fa5ffb0
silindammn <mduduzis@automationexchange.co.za>
2026-06-23 11:53:54 +0200

    register login ui

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { AuthPageComponent } from "./auth/auth-page/auth-page.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, AuthPageComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
