7c6f8bbc6a385fe3ccdb62a6b3e8d7d567243c71
silindammn <mduduzis@automationexchange.co.za>
2026-07-01 13:07:36 +0200

    push these

--- src/app/services/hr/jobtitles.service.ts
+++ src/app/services/hr/jobtitles.service.ts
@@ -3,10 +3,16 @@ import { HttpClient } from '@angular/common/http';
 import { Client } from '../../models/hr/client';
 import { GenericCrudService } from '../generic.service';
 import { JobTitle } from '../../models/hr/jobtitle';
+import { Observable } from 'rxjs';
 
 @Injectable({ providedIn: 'root' })
 export class JobTitleService extends GenericCrudService<JobTitle> {
   constructor(http: HttpClient) {
     super(http, 'JobTitle');
+  } 
+  getJobTitleByDepartmentId(departmentId: number): Observable<JobTitle[]> {
+    return this.http.get<JobTitle[]>(
+      `${this.baseUrl}/Department/${departmentId}`
+    );
   }
 }
