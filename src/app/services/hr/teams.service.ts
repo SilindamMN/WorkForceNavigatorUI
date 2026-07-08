c489a43303432896f93f9df0030995e03f4efbd0
silindammn <silindanemdu@gmail.com>
2026-07-08 18:51:48 +0200

    fixing user teams

--- src/app/services/hr/teams.service.ts
+++ src/app/services/hr/teams.service.ts
@@ -2,11 +2,18 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Client } from '../../models/hr/client';
 import { GenericCrudService } from '../generic.service';
-import { Team } from '../../models/hr/team';
+import { Team, UserTeamListDto } from '../../models/hr/team';
+import { Observable } from 'rxjs';
 
 @Injectable({ providedIn: 'root' })
 export class TeamsService extends GenericCrudService<Team> {
   constructor(http: HttpClient) {
     super(http, 'Team');
   }
+    getUserTeamByUserId(userId: string): Observable<UserTeamListDto[]> {
+      return this.http.get<UserTeamListDto[]>(
+        `${this.baseUrl}/user/${userId}`
+      );
+    }
+    
 }
